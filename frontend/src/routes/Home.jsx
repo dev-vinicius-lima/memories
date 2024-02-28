3c3e0741e6d5709345ad69c1258888d0f814d05d
Vinicius Lima <viniciuslimaes@hotmail.com>
2024-02-28 17:10:37 -0400

    style page home

--- frontend/src/routes/Home.jsx
+++ frontend/src/routes/Home.jsx
@@ -1,7 +1,36 @@
+import axios from "../axios-config.js";
+import { useState, useEffect } from "react";
+import "./Home.modules.css";
+import { Link } from "react-router-dom";
 const Home = () => {
+  const [memories, setMemories] = useState([]);
+
+  useEffect(() => {
+    const getMemories = async () => {
+      const res = await axios.get("/memories");
+
+      setMemories(res.data);
+    };
+    getMemories();
+  }, []);
   return (
-    <div>
-      <h1>Home</h1>
+    <div className="home">
+      <h2>Confira as ultimas memórias</h2>
+      <div className="memories_container">
+        {memories.length > 0 &&
+          memories.map((memory) => (
+            <div key={memory._id} className="memory">
+              <img
+                src={`${axios.defaults.baseURL}${memory.src}`}
+                alt={memory.title}
+              />
+              <p>{memory.title}</p>
+              <Link className="btn" to={`/${memory._id}`}>
+                Comentar
+              </Link>
+            </div>
+          ))}
+      </div>
     </div>
   );
 };
