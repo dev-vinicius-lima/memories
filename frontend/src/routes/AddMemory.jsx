5372a651e7d49b311ed037b11e7c5f3d2d4fe6e6
Vinicius Lima <viniciuslimaes@hotmail.com>
2024-02-28 15:34:08 -0400

    Create style to form

--- frontend/src/routes/AddMemory.jsx
+++ frontend/src/routes/AddMemory.jsx
@@ -1,7 +1,27 @@
+import "./AddMemory.modules.css";
+
 const AddMemory = () => {
   return (
-    <div>
-      <h1>AddMemory</h1>
+    <div className="add_memory_page">
+      <h2>Crie uma nova memória</h2>
+      <form>
+        <label>
+          <p>Título:</p>
+          <input type="text" placeholder="Defina um título" name="title" />
+        </label>
+        <label>
+          <p>Descrição:</p>
+          <textarea
+            placeholder="Explique oque aconteceu..."
+            name="description"
+          ></textarea>
+        </label>
+        <label>
+          <p>Foto:</p>
+          <input type="file" name="image" />
+        </label>
+        <input type="submit" value="Enviar" className="btn" />
+      </form>
     </div>
   );
 };
